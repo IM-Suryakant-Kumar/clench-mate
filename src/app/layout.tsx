14e8e3560d8a200fe12f19bd497c5195737911f5
IM-Suryakant-Kumar <ksuryakant43@gmail.com>
2025-09-03 20:06:01 +0530

    feat: Added Header

---
+++ src/app/layout.tsx
@@ -0,0 +1,30 @@
+import "@/styles/globals.css";
+
+import { type Metadata } from "next";
+import { Josefin_Sans, Cinzel } from "next/font/google";
+
+export const metadata: Metadata = {
+  title: "Clench Mate",
+  description: "A full stack social media app where you can explore people and communities.And share your thoughts.",
+  icons: [{ rel: "icon", url: "/favicon.ico" }],
+};
+
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  variable: "--font-josefin",
+});
+
+const cinzel = Cinzel({
+  subsets: ["latin"],
+  variable: "--font-cinzel",
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <html lang="en" className={`${josefin.variable} ${cinzel.variable}`}>
+      <body>{children}</body>
+    </html>
+  );
+}
---
+++ src/components/Header.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <header className="bg-secondary fixed top-0 left-0 flex h-25 w-full flex-wrap items-center justify-between gap-2 px-4 py-2 md:h-16">
+      <Link href="/home" className="flex items-center gap-1">
+        <Image src="/logo.png" alt="Logo" width={30} height={30} />
+        <h1 className="text-logo text-xl font-bold">ClenchMate</h1>
+      </Link>
+      <div className="border-logo h-8 w-8 rounded-full border-2 md:order-3">
+        <Image src="/logo.png" alt="User Avatar" width={32} height={32} />
+      </div>
+      <div className="h-8 w-full overflow-hidden rounded-md border-2 border-gray-400 md:max-w-sm">
+        <input
+          type="search"
+          className="bg-primary h-full w-full px-2 outline-none"
+          placeholder="Search..."
+        />
+      </div>
+    </header>
+  );
+}
