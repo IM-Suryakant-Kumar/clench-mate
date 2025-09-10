9c9b70ab78f550ba513e511ac9d40685b26e77c2
IM-Suryakant-Kumar <ksuryakant43@gmail.com>
2025-09-10 12:17:51 +0530

    feat: Added not-found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,7 @@
+export default function NotFound() {
+  return (
+    <main className="w-full h-screen flex justify-center items-center text-center">
+      <p className="text-gray-600 text-xl font-bold md:text-2xl">404 - PAGE NOT FOUND!</p>
+    </main>
+  )
+}
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,7 +1,39 @@
-const Sidebar = () => {
-  return (
-    <div>Sidebar</div>
-  )
-}
+"use client";
 
-export default Sidebar
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import {
+	MdOutlineExplore,
+	MdOutlineHome,
+	MdOutlinePerson,
+	MdOutlineSettings,
+} from "react-icons/md";
+
+const Sidebar = () => {
+	const pathname = usePathname();
+	const links = [
+		{ name: "Home", href: "/home", icon: MdOutlineHome },
+		{ name: "Explore", href: "/explore", icon: MdOutlineExplore },
+		{ name: "Profile", href: "/profile", icon: MdOutlinePerson },
+		{ name: "Settings", href: "/settings", icon: MdOutlineSettings },
+	];
+
+	return (
+		<aside className="w-full h-16 bg-secondary fixed left-0 bottom-0 flex justify-between items-center gap-2 p-4">
+			{links.map((link) => (
+				<Link
+					key={link.name}
+					href={link.href}
+					className={`${
+						pathname === link.href ? "border-2 border-logo rounded-full" : ""
+					}`}
+				>
+					<span className="hidden">{link.name}</span>
+					<link.icon size="1.5em" className="text-logo" />
+				</Link>
+			))}
+		</aside>
+	);
+};
+
+export default Sidebar;
