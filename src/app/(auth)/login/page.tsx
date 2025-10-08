0da36c3a47e44e74c98a0325d0655add0a9da044
IM-Suryakant-Kumar <ksuryakant43@gmail.com>
2025-10-08 16:57:53 +0530

    feat: basic setup

--- src/app/(auth)/login/page.tsx
+++
@@ -1,7 +0,0 @@
-import Login from "@/components/Login";
-
-export default function page() {
-	return <div className="min-h-screen flex justify-center items-center">
-    <Login />
-  </div>
-}
--- src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default async function Home() {
-	return (
-		<div className="">
-			<h1>Home</h1>
-		</div>
-	);
-}
