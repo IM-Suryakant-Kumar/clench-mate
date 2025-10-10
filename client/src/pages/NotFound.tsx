9ba6c9c8b527c6d7ca940357b0e3e2e3ddd29ea1
IM-Suryakant-Kumar <ksuryakant43@gmail.com>
2025-10-10 14:00:45 +0530

    feat: implemented NotFound page

--- client/src/pages/NotFound.tsx
+++ client/src/pages/NotFound.tsx
@@ -2,5 +2,9 @@ import { useDocumentTitle } from "../hooks";
 
 export const NotFound = () => {
 	useDocumentTitle("Not Found");
-	return <div>NotFound</div>;
+	return (
+		<div className="min-h-screen flex justify-center items-center">
+			<h1 className="text-red-500 text-xl md:text-3xl font-semibold text-center">404 - PAGE NOT FOUND!</h1>
+		</div>
+	);
 };
