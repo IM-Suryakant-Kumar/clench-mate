6b195932ad6946d7d8c3af74a845900df6a188ec
IM-Suryakant-Kumar <ksuryakant43@gmail.com>
2025-10-10 14:09:54 +0530

    feat: updated NotFound page

--- client/src/pages/NotFound.tsx
+++ client/src/pages/NotFound.tsx
@@ -1,10 +1,20 @@
+import { Link } from "react-router";
 import { useDocumentTitle } from "../hooks";
 
 export const NotFound = () => {
 	useDocumentTitle("Not Found");
 	return (
-		<div className="min-h-screen flex justify-center items-center">
-			<h1 className="text-red-500 text-xl md:text-3xl font-semibold text-center">404 - PAGE NOT FOUND!</h1>
+		<div className="min-h-screen flex flex-col justify-center items-center gap-6">
+			<h1 className="text-red-500 font-cinzel text-xl md:text-3xl font-semibold text-center">
+				404 - PAGE NOT FOUND!
+			</h1>
+			<Link
+				to="/"
+				className="bg-logo text-primary font-semibold px-10 py-2 rounded-xs"
+				replace
+			>
+				Go To Home
+			</Link>
 		</div>
 	);
 };
